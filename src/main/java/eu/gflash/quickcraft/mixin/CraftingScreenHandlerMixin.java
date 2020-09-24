2f335e93213308c3feaa6c3d4617088eba3f40ae
Alex811 <811alex@gmail.com>
2020-09-24 19:44:11 +0300

    Patch for multiplayer

--- src/main/java/eu/gflash/quickcraft/mixin/CraftingScreenHandlerMixin.java
+++ src/main/java/eu/gflash/quickcraft/mixin/CraftingScreenHandlerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.CraftingResultInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.CraftingScreenHandler;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +16,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(CraftingScreenHandler.class)
 public class CraftingScreenHandlerMixin {
 
-	@Inject(method = "updateResult", at = @At("RETURN"))
-	private static void updateResult(int syncId, World world, PlayerEntity player, CraftingInventory craftingInventory, CraftingResultInventory resultInventory, CallbackInfo ci){
+	@Inject(method = "onContentChanged", at = @At("RETURN"))
+	private void onContentChanged(Inventory inventory, CallbackInfo ci){
 		if(Screen.hasControlDown()) {
 			InventoryHelper.scheduleCraft();
 		}
