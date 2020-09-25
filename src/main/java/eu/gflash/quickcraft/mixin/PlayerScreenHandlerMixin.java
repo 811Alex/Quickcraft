3dbcbdc1b22dac160993770f9d4dfc3cf27b219d
Alex811 <811alex@gmail.com>
2020-09-26 01:00:36 +0300

    Re-add player crafting screen support

---
+++ src/main/java/eu/gflash/quickcraft/mixin/PlayerScreenHandlerMixin.java
@@ -0,0 +1,21 @@
+package eu.gflash.quickcraft.mixin;
+
+import eu.gflash.quickcraft.client.InventoryHelper;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.screen.PlayerScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerScreenHandler.class)
+public class PlayerScreenHandlerMixin {
+
+    @Inject(method = "onContentChanged", at = @At("RETURN"))
+    private void onContentChanged(Inventory inventory, CallbackInfo ci){
+        if(Screen.hasControlDown()) {
+            InventoryHelper.scheduleCraft();
+        }
+    }
+}
